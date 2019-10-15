8b7a977c6e7278590a09cb7c87e065257f8fe688
John Broderick <johnbroderick12@gmail.com>
2019-10-15 14:36:54 -0400

    when there doesn't exist a `db.info` file, creates one and adds dummy information so it doesn't crash the applet

--- src/utility.py
+++ src/utility.py
@@ -4,6 +4,13 @@ def load_database_info():
         db_config = open('../db.info', 'r')
     except IOError:
         print('ERROR: Create a file called \'db.info\' in the root directory...exiting')
+        db_config = open('../db.info', 'w')
+        db_config.write('secretkey\n')
+        db_config.write('localhost\n')
+        db_config.write('root\n')
+        db_config.write('password\n')
+        db_config.write('database\n')
+        db_config.close()
         return None
 
     info['secret_key'] = db_config.readline().strip()
