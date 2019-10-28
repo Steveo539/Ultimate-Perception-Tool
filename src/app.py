4aaaecf0a05bb4aa28082f468d97d44465b7698d
Senarii <Vortekdynamic@gmail.com>
2019-10-28 14:44:32 -0400

    Add title field to layout html page

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Index")
 
 
 @app.route("/create_form")
