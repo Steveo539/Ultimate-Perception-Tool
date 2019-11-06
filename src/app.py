0ba90d7b9d583aee654cf43268b66ffcc1bf502c
John Broderick <johnbroderick12@gmail.com>
2019-11-06 14:06:31 -0500

    creates "user" table before first request
    gen_user creates a test user on first link
    
    can now login and logout with the test user

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from utility import *
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
+from access import *
 
 app = Flask(__name__)
 
@@ -10,50 +11,87 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", title="Index")
 
+# Table sql line: CREATE TABLE users(id INT(12) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30), password VARCHAR(100))
+# Creates a test user with username: manager and password: test
+@app.route("/gen_user")
+def create_user():
+    cur = mysql.connection.cursor()
+    res = cur.execute("SELECT * FROM users WHERE username=%s", ["manager"])
+    if res > 0:
+        cur.close()
+        return "Already created user"
+    else:
+        username = "manager"
+        password = sha256_crypt.encrypt("test")
+        cur.execute("INSERT INTO users(username, password) VALUES(%s, %s)", (username, password))
+        mysql.connection.commit()
+        cur.close()
+        return "Created user"
+
 
 @app.route("/login", methods=["GET", "POST"])
+@is_logged_out
 def login():
-
-    HAVE_NOT_FIXED_DATABASE_YET = True
-
-    if request.method == "POST" and not HAVE_NOT_FIXED_DATABASE_YET:
+    if request.method == "POST":
         username = request.form["username"]
         pass_candidate = request.form["password"]
         cur = mysql.connection.cursor()
         res = cur.execute("SELECT * FROM users WHERE username=%s", [username])
-        cur.close()
         if res > 0:
             user = cur.fetchone()
+            cur.close()
             password = user['password']
 
             if sha256_crypt.verify(pass_candidate, password):
                 session['logged_in'] = True
                 session['username'] = user['username']
+                return redirect(url_for("index"))
             else:
                 error = "Invalid User or Password"
                 return render_template("login.html", error=error)
         else:
+            cur.close()
             error = "Invalid User or Password"
             return render_template("login.html", error=error)
     else:
         return render_template("login.html")
 
 
+@app.route("/logout")
+@is_logged_in
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
 @app.route("/forms/")
 @app.route("/forms/index")
+@is_logged_in
 def view_forms():
     return render_template("survey/index.html", title="Survey Home")
 
 
 @app.route("/forms/new")
+@is_logged_in
 def create_form():
     return render_template("survey/new.html", title="Create New Survey")
 
 
 @app.route("/forms/view")
+@is_logged_in
 def view_form():
     return render_template("survey/view.html", title="Survey Detail")
 
+
+@app.before_first_request
+def create_tables():
+    cur = mysql.connection.cursor()
+    res = cur.execute("SHOW TABLES LIKE \'users\'")
+    if res < 1:
+        print("Creating user table...")
+        cur.execute("CREATE TABLE users(id INT(12) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30), password VARCHAR(100))")
+        mysql.connection.commit()
+    cur.close()
 
 if __name__ == "__main__":
     info = load_database_info()
