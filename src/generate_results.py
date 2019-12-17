7efa9279728e68490159c8c2ab26aa226baa8275
lukeyeh <lukeoverrhere@gmail.com>
2019-12-17 17:33:09 -0500

    change my surveys

--- src/generate_results.py
+++ src/generate_results.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from collections import OrderedDict
 
 dummy_multiple_choice_data = {
     'title': 'Why do you hate your manager?',
     'type': 'multiple_choice',
-    'optionList': ('He is ugly', 'His breath smells bad', 'He does not take showers'),
-    'results': [10, 20, 30] # index of results represent
+    'optionList': OrderedDict({'He is ugly': 10, 'His breath smells bad': 20, 'He does not take showers': 30}),
 }
 
 dummy_rating_choice_data = {
@@ -19,29 +19,13 @@ dummy_rating_choice_data = {
 
 
 def generate_multiple_choice(data, save=False):
-    x_axis = data['optionList']
+    x_axis = data['optionList'].keys()
     y_pos = np.arange(len(x_axis))
-    plt.bar(y_pos, data['results'], align='center', alpha=0.5)
+    plt.bar(y_pos, list(data['optionList'].values()), align='center', alpha=0.5)
     plt.xticks(y_pos, x_axis)
     plt.ylabel('# (frequency)')
-    plt.xlabel('choice)')
+    plt.xlabel('(choice)')
     plt.title(data['title'])
-    matplotlib.rc('xtick', labelsize=20)
-    matplotlib.rc('ytick', labelsize=20)
-    if (save):
-        return plt.savefig('books_read.png')
-    plt.show()
-
-
-def generate_rating(data, save=False):
-    print(range(*data['rating_choice_range']))
-    x_axis = tuple(range(*data['rating_choice_range']))
-    y_pos = np.arange(len(x_axis))
-    plt.bar(y_pos, data['results'], align='center', alpha=0.5)
-    plt.xticks(y_pos, x_axis)
-    plt.ylabel('# (frequency)')
-    plt.xlabel('rating')
-    plt.title(data['question_title'])
     matplotlib.rc('xtick', labelsize=20)
     matplotlib.rc('ytick', labelsize=20)
     if (save):
